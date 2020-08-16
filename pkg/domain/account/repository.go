b153020c14b8e6b9f4e6aef818027bab41120491
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-16 14:46:00 -0300

    Pass the account by reference in account.Repository.Store

--- pkg/domain/account/repository.go
+++ pkg/domain/account/repository.go
@@ -2,7 +2,7 @@ package account
 
 // Repository manages account persistence.
 type Repository interface {
-	Store(Account) error
+	Store(*Account) error
 	UpdateBalance(Account) error
 	GetByID(string) (Account, error)
 	ListAll() ([]Account, error)
