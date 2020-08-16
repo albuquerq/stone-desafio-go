f267a35e7a8d76ccfbb78740fb5ffd5ea9fd9f77
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-16 12:59:38 -0300

    Create domain errors

---
+++ pkg/domain/errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Domain errors.
+var (
+	ErrDomain                   = errors.New("domain error")
+	ErrInsufficientBalance      = fmt.Errorf("%w: %v", ErrDomain, "insufficient balance")
+	ErrInvalidAccessCredentials = fmt.Errorf("%w: %v", ErrDomain, "invalid access credentials")
+)
