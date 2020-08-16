1c46c72e5bd9125a9862f254d1a1e1bde92cc94d
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-16 18:46:09 -0300

    Refactor domain errors

--- pkg/domain/errors/errors.go
+++ pkg/domain/errors/errors.go
@@ -7,9 +7,12 @@ import (
 
 // Domain errors.
 var (
-	ErrDomain                   = errors.New("domain error")
-	ErrInsufficientBalance      = fmt.Errorf("%w: %v", ErrDomain, "insufficient balance")
-	ErrInvalidAccessCredentials = fmt.Errorf("%w: %v", ErrDomain, "invalid access credentials")
-	ErrNoHasUniqueIdentity      = fmt.Errorf("%w: %v", ErrDomain, "no has unique identity")
-	ErrAccountNotFound          = fmt.Errorf("%w: %v", ErrDomain, "account not found")
+	ErrDomain                      = errors.New("domain error")
+	ErrInvalidAccessCredentials    = fmt.Errorf("%w: %v", ErrDomain, "invalid access credentials")
+	ErrNoHasUniqueIdentity         = fmt.Errorf("%w: %v", ErrDomain, "no has unique identity")
+	ErrAccountNotFound             = fmt.Errorf("%w: %v", ErrDomain, "account not found")
+	ErrTransferNotAllowed          = fmt.Errorf("%w: %v", ErrDomain, "transfer not allowed")
+	ErrTransferInsufficientBalance = fmt.Errorf("%w: %v", ErrTransferNotAllowed, "the origin account has insufficient balance")
+	ErrTransferMissingAmount       = fmt.Errorf("%w: %v", ErrTransferNotAllowed, "missing amount")
+	ErrTransferBetweenSameAccount  = fmt.Errorf("%w: %v", ErrTransferNotAllowed, "transfer between the same account")
 )
