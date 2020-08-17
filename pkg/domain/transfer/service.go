69bb92c9d79058810770dac0659630ef8fd3f007
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-17 13:08:49 -0300

    Add the amount parameter to the transfer.Service.Transfer method

--- pkg/domain/transfer/service.go
+++ pkg/domain/transfer/service.go
@@ -4,6 +4,6 @@ import "github.com/albuquerq/stone-desafio-go/pkg/domain/account"
 
 // Service encapsulates domain operations over the transfer model as a service.
 type Service interface {
-	Transfer(from account.Account, to account.Account) (Transfer, error)
+	Transfer(from account.Account, to account.Account, amount int) (Transfer, error)
 	ListTransfersFromAccount(accountID string) ([]Transfer, error)
 }
