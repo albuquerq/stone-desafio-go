c71096b9109f5a76d640471a83132aff36105a16
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-18 01:59:09 -0300

    Create a value object for access service

--- pkg/domain/access/service.go
+++ pkg/domain/access/service.go
@@ -1,8 +1,6 @@
 package access
 
-import "github.com/albuquerq/stone-desafio-go/pkg/domain/account"
-
 // Service responsible for authentication.
 type Service interface {
-	Authenticate(Credential) (account.Account, error)
+	Authenticate(Credential) (Description, error)
 }
--- pkg/domain/access/value.go
+++ pkg/domain/access/value.go
@@ -5,3 +5,10 @@ type Credential struct {
 	CPF    string
 	Secret string
 }
+
+// Description is the value object that describes account ownership information.
+type Description struct {
+	Name      string
+	CPF       string
+	AccountID string
+}
