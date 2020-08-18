6a1d17b884a02597bd1df6af7d011724cede0922
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-18 02:09:56 -0300

    Change the transfer method parameters to account.Service

--- pkg/domain/transfer/service.go
+++ pkg/domain/transfer/service.go
@@ -1,9 +1,7 @@
 package transfer
 
-import "github.com/albuquerq/stone-desafio-go/pkg/domain/account"
-
 // Service encapsulates domain operations over the transfer model as a service.
 type Service interface {
-	Transfer(from account.Account, to account.Account, amount int) (Transfer, error)
+	Transfer(accountOringinID string, accountDestinationID string, amount int) (Transfer, error)
 	ListTransfersFromAccount(accountID string) ([]Transfer, error)
 }
