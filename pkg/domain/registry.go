1c6072081e4b8b1e2c55e64e1db274c75c1db076
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-21 00:22:56 -0300

    Create domain registries

---
+++ pkg/domain/registry.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"database/sql/driver"
+
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/access"
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/account"
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/transfer"
+)
+
+// RepositoryRegistry group repository factories.
+type RepositoryRegistry interface {
+	AccountRepository() account.Repository
+	TransferRepository() transfer.Repository
+	Tx() driver.Tx
+}
+
+// ServiceRegistry group service factories.
+type ServiceRegistry interface {
+	AccessService() access.Service
+	AccountService() account.Service
+	TransferService() transfer.Service
+}
