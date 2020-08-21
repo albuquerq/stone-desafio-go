ee53175857afd165c346082a15b90e7823fd8249
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-21 01:06:53 -0300

    Implements access service at the application layer

---
+++ pkg/application/access_service.go
@@ -0,0 +1,51 @@
+package application
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/access"
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/account"
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/errors"
+	"github.com/albuquerq/stone-desafio-go/pkg/infraestructure/common"
+	"github.com/albuquerq/stone-desafio-go/pkg/infraestructure/validator"
+)
+
+type accessService struct {
+	log               *logrus.Entry
+	accountRepository account.Repository
+}
+
+// NewAccessService returns a new access service.
+func NewAccessService(acRepository account.Repository) access.Service {
+	return &accessService{
+		log:               common.Logger().WithField("source", "accessService"),
+		accountRepository: acRepository,
+	}
+}
+
+func (as *accessService) Authenticate(cred access.Credential) (d access.Description, err error) {
+	log := as.log.WithField("op", "Authenticate").WithField("cpf", cred.CPF)
+
+	err = validator.ValidateAccessCredential(cred)
+	if err != nil {
+		return
+	}
+
+	ac, err := as.accountRepository.GetByCPF(cred.CPF)
+	if err != nil {
+		log.WithError(err).Error("no account found with this cpf")
+		return
+	}
+
+	if !common.MatchHashAndSecret(ac.Secret, cred.Secret) { // match secrets using hash.
+		err = errors.ErrInvalidAccessCredentials
+		log.WithError(err).Error("mismatch secrets")
+		return
+	}
+
+	d.AccountID = ac.ID
+	d.CPF = ac.CPF
+	d.Name = ac.Name
+
+	return
+}
---
+++ pkg/application/access_service_test.go
@@ -0,0 +1,47 @@
+package application
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/access"
+	"github.com/albuquerq/stone-desafio-go/pkg/domain/account"
+	"github.com/albuquerq/stone-desafio-go/pkg/infraestructure/common"
+)
+
+var (
+	accessServiceVar access.Service = NewAccessService(repoRegistry.AccountRepository()) // repoRegistry defined in "account_service_test.go" file.
+)
+
+func TestAcessService(t *testing.T) {
+
+	ac := account.Account{
+		ID:      common.GenUUID(),
+		Name:    "Jhon Doe Acess",
+		Balance: 500050,
+		CPF:     "76312890312",
+		Secret:  "secret pass 1",
+	}
+
+	// Create account for test.
+	err := acService.CreateAccount(&ac)
+	assert.NoError(t, err)
+
+	acDescription, err := accessServiceVar.Authenticate(access.Credential{
+		CPF:    "76312890312",
+		Secret: "err secret",
+	})
+	assert.Error(t, err)
+
+	acDescription, err = accessServiceVar.Authenticate(access.Credential{
+		CPF:    "76312890312",
+		Secret: "secret pass 1",
+	})
+	assert.NoError(t, err)
+
+	assert.Equal(t, ac.ID, acDescription.AccountID)
+	assert.Equal(t, "76312890312", acDescription.CPF)
+	assert.Equal(t, ac.Name, acDescription.Name)
+
+}
