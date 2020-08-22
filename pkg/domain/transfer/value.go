4e75b4b514324ba594ed64b3c1b0b968ff06f8e9
Natan Albuquerque <natanalbuquerque5@gmail.com>
2020-08-22 12:54:03 -0300

    Remove not used field in transfer input value

--- pkg/domain/transfer/value.go
+++ pkg/domain/transfer/value.go
@@ -2,7 +2,6 @@ package transfer
 
 // InputValue to create a transfer.
 type InputValue struct {
-	AccountOriginID      string `json:"account_origin_id"`
 	AccountDestinationID string `json:"account_destination_id"`
 	// Amount in Brazilian real cents BLR..
 	Amount int `json:"amount"`
